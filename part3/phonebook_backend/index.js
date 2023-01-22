bcffa9c6709aeb915b6c7ec8e9a5d0f8ef92db18
Yevhenii Chernenko <105671194+malighters@users.noreply.github.com>
2023-01-22 16:38:43 +0200

    Add files via upload

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,125 @@
+/* eslint-disable linebreak-style */
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const Person = require('./models/person')
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
+    response.send(`Phonebook has info for ${persons.length} people </br>  
+        ${new Date()}`)
+  })
+
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  Person.find({}).then(persons => {
+    if(persons.find(person => person.name === body.name)){
+      return response.status(400).json({
+        error: 'name must be unique'
+      })
+    }})
+
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+app.put('/api/persons:id', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => {
+      next(error)
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
